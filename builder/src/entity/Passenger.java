a19edb3528410d6c545fbf1cd06f91c0ffdc0f34
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2022-05-03 22:39:42 +0000

    2-lab: fix

--- builder/src/entity/Passenger.java
+++ builder/src/entity/Passenger.java
@@ -1,18 +1,9 @@
 package entity;
 
 public class Passenger {
-    PassengerStatus status = PassengerStatus.wait;
     PassengerType type;
 
     public Passenger(PassengerType type) {
         this.type = type;
-    }
-
-    public void setStatus(PassengerStatus status) {
-        this.status = status;
-    }
-
-    public PassengerType getPassType() {
-        return type;
     }
 }
